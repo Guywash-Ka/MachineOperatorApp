ed7e372a508e09e7857b88816633a3d21ff032c6
Guywash-Ka <rodvlasov2003@yandex.ru>
2023-11-16 23:35:25 +0300

    Updated worker logic

--- app/src/main/java/com/example/mechanicoperatorapp/MainActivity.kt
+++ app/src/main/java/com/example/mechanicoperatorapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mechanicoperatorapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,11 +12,37 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
 import com.example.mechanicoperatorapp.ui.theme.MechanicOperatorAppTheme
+import com.example.mechanicoperatorapp.worker.DownloadWorker
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.toDuration
+import kotlin.time.toJavaDuration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Worker start
+        val channel =
+            NotificationChannel("default", "Default", NotificationManager.IMPORTANCE_DEFAULT)
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+
+        val downloadWorkRequest: WorkRequest = PeriodicWorkRequest.Builder(DownloadWorker::class.java, 15.minutes.toJavaDuration())
+            .setConstraints(Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.CONNECTED)
+                    .build())
+            .build()
+
+        WorkManager.getInstance(this).enqueue(downloadWorkRequest)
+
+        // Worker end
+
         setContent {
             MechanicOperatorAppTheme {
                 // A surface container using the 'background' color from the theme
--- app/src/main/java/com/example/mechanicoperatorapp/worker/DownloadWorker.kt
+++ app/src/main/java/com/example/mechanicoperatorapp/worker/DownloadWorker.kt
@@ -1,6 +1,7 @@
 package com.example.mechanicoperatorapp.worker
 
 import android.content.Context
+import androidx.core.app.NotificationCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.mechanicoperatorapp.data.AppRepository
@@ -13,6 +14,11 @@ class DownloadWorker(context: Context, params: WorkerParameters) : Worker(contex
     override fun doWork(): Result {
         try {
             AppRepository.get().getData()
+            val notification = NotificationCompat.Builder(applicationContext, "default")
+                .setContentTitle("Новые данные загружены")
+                .setContentText("Возможно Вы получили новые задания, скорее проверьте!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build()
         } catch (e: Exception) {
             return Result.failure()
         }
