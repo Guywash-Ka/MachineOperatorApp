eba089d9452c418f8d9432d9a1ecb736f4fa6a3c
vladesire <vladesire2.0@gmail.com>
2023-11-18 12:47:00 +0300

    Roles UI logic

--- app/src/main/java/com/example/mechanicoperatorapp/MainActivity.kt
+++ app/src/main/java/com/example/mechanicoperatorapp/MainActivity.kt
@@ -10,17 +10,42 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.interaction.Interaction
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequest
@@ -32,15 +57,25 @@ import com.example.mechanicoperatorapp.data.dataClasses.WorkerEntity
 import com.example.mechanicoperatorapp.ui.theme.MechanicOperatorAppTheme
 import com.example.mechanicoperatorapp.worker.DownloadWorker
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.update
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.toJavaDuration
 
-private data class Worker(
-    val id: Int,
-    val role: Int,
-)
+class NoRippleInteractionSource : MutableInteractionSource {
+    override val interactions: Flow<Interaction> = emptyFlow()
+    override suspend fun emit(interaction: Interaction) {}
+    override fun tryEmit(interaction: Interaction) = true
+}
+
+sealed class Screen(val route: String, @StringRes val name: Int) {
+    object Messages : Screen("rides", R.string.messages_screen)
+    object Tasks : Screen("main", R.string.tasks_screen)
+    object Profile : Screen("profile", R.string.profile_screen)
+    object AddTask : Screen("add-task", R.string.add_task_screen)
+}
 
 private data class MainActivityState(
     val showSplash: Boolean = false,
@@ -58,6 +93,7 @@ class MainActivity : ComponentActivity() {
     )
 
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -92,60 +128,143 @@ class MainActivity : ComponentActivity() {
 
             val mainState by mainStateFlow.collectAsStateWithLifecycle()
 
+            val navController = rememberNavController()
 
-            when {
-                mainState.showSplash -> {
 
-                }
+            MechanicOperatorAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
 
-                !mainState.isLoggedIn -> {
+                    when {
+                        mainState.showSplash -> {
 
-                    Column() {
-                        Text("LOGIN SCREEN")
+                        }
 
-                        if (mainState.nfcSerialNumber != null) {
-                            LaunchedEffect(null) {
+                        !mainState.isLoggedIn -> {
 
-                                val worker = repo.getProfileByNfc(mainState.nfcSerialNumber!!)
+                            Column() {
+                                Text("LOGIN SCREEN")
 
-                                if (worker.id != -1) {
-                                    mainStateFlow.value = MainActivityState(false, true, null, worker)
+                                if (mainState.nfcSerialNumber != null) {
+                                    LaunchedEffect(null) {
+
+                                        val worker = repo.getProfileByNfc(mainState.nfcSerialNumber!!)
+
+                                        if (worker.id != -1) {
+                                            mainStateFlow.value = MainActivityState(false, true, null, worker)
+                                        }
+
+                                    }
+                                }
+                            }
+
+                        }
+
+                        else -> {
+
+                            // INSIDE APPLICATION
+
+
+
+
+                            Scaffold(
+                                bottomBar = {
+                                    NavigationBar(
+                                        containerColor = Color.White
+                                    ) {
+                                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                                        val currentDestination = navBackStackEntry?.destination
+
+                                        // TODO: DIVIDE BY TYPES
+                                        val screens = listOf(
+                                            Screen.Messages,
+                                            Screen.Tasks,
+                                            Screen.Profile
+                                        )
+
+                                        screens.forEach { screen ->
+
+                                            NavigationBarItem(
+                                                label = {
+                                                    Text(
+                                                        text = stringResource(screen.name),
+                                                        fontSize = 10.sp,
+                                                    )
+                                                },
+                                                selected =
+                                                currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                                                onClick = {
+
+//                                                    Log.e(TAG, "Click: $screen}")
+
+                                                    navController.navigate(screen.route) {
+                                                        popUpTo(navController.graph.findStartDestination().id) {
+                                                            saveState = true
+                                                        }
+                                                        launchSingleTop = true
+                                                        restoreState = true
+                                                    }
+                                                },
+                                                icon = {
+
+                                                    Icon(
+                                                        Icons.Rounded.AccountBox,
+                                                        contentDescription = null
+                                                    )
+
+//                                                    when (screen) {
+//                                                        Screen.Main -> Image(
+//                                                            painterResource(R.drawable.ic_main),
+//                                                            contentDescription = null,
+//                                                            modifier = Modifier.size(24.dp)
+//                                                        )
+//
+//                                                        Screen.Profile -> Image(
+//                                                            painterResource(R.drawable.ic_profile),
+//                                                            contentDescription = null,
+//                                                            modifier = Modifier.size(24.dp)
+//                                                        )
+//
+//
+//                                                        Screen.Rides -> Image(
+//                                                            painterResource(R.drawable.ic_rides),
+//                                                            contentDescription = null,
+//                                                            modifier = Modifier.size(24.dp)
+//                                                        )
+//                                                    }
+                                                },
+                                                colors = NavigationBarItemDefaults.colors(
+                                                    selectedIconColor = Color(0xFFFFA96B),
+                                                    selectedTextColor = Color(0xFFFFA96B),
+                                                    unselectedTextColor = Color(0xFFA8A8A8),
+                                                    indicatorColor = Color.White
+                                                ),
+                                                interactionSource = NoRippleInteractionSource()
+                                            )
+                                        }
+                                    }
+                                }
+                            ) { innerPadding ->
+
+                                NavHost(
+                                    navController,
+                                    startDestination = Screen.Tasks.route,
+                                    Modifier.padding(innerPadding)
+                                ) {
+                                    composable(Screen.Messages.route) { Text("MESSAGES SCREEN") }
+                                    composable(Screen.Tasks.route) { Text("TASKS SCREEN") }
+                                    composable(Screen.Profile.route) { Text("PROFILE SCREEN") }
+                                    composable(Screen.AddTask.route) { Text("ADD TASK SCREEN") }
                                 }
 
                             }
                         }
                     }
-
-                }
-
-                else -> {
-
-                    Column {
-                        Text("INSIDE APPLICATION")
-                        // mainState.worker is not null here
-
-
-                        Text("Role: ${mainState.worker!!.role}")
-                        Text("ID: ${mainState.worker!!.id}")
-
-                    }
-
-
                 }
             }
 
-
-
-
-//            MechanicOperatorAppTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
         }
     }
 
@@ -185,20 +304,4 @@ class MainActivity : ComponentActivity() {
         nfcAdapter?.enableForegroundDispatch(this, nfcPendingIntent, null, null)
     }
 
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MechanicOperatorAppTheme {
-        Greeting("Android")
-    }
 }
